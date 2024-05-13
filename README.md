5a011e2b84d9e6d64aecec87347c3c25d3fb12b0
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-05-13 08:03:38 +0530

    Async TASK-2 -- USA Data API - Displaying USA Population Data

---
+++ README.md
@@ -0,0 +1,19 @@
+# Promise-API TASK2
+
+***Purpose of the Project:***   
+By accessing the *AmiiboAPI*, we have to display the Amiibo series and the Characters along with image using HTML,CSS,Bootstrap,Javascript
+
+***Design:***   
+- Data fetched from API   
+- CSS styles with Class selector   
+- Bootstrap Card to display the contents  
+
+***Accompolished with:***
+- Used **fetch() API** to get the data from AmiiboAPI   
+- Used try/catch for error handling while fetching data from API
+- Used **async & await** to retreive the Promise Data 
+- Used Container to load the contents with breakpoint *col-md-4*   
+- User *for loop* to iterate the amiibo data and loaded into each cards along with an image
+- Used Bootstrap cards to display the necessary data on the screen   
+
+***Author:Tharani K***   
---
+++ script.js
@@ -0,0 +1,36 @@
+async function apiCall(){
+    try{
+        let api_content = await fetch("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
+        let usa_apival = await api_content.json()
+        let usa_data = usa_apival.data
+        let data = []
+        let parent_div = document.createElement("div")
+        parent_div.className = "par_div"
+        parent_div.innerHTML = `<div class="card bg-primary mb-3" style="width: 18rem;">
+        <div class="card-header">
+        <b>USA Population Data</b>
+        </div>
+        <ul class="list-group list-group-flush">
+    
+    ${
+        (function iterate_item(){
+            
+            for(let i=0;i<usa_data.length;i++){
+               
+                console.log("inside for")
+                data.push (`<li class="list-group-item"><center>Year:<b> ${usa_data[i].Year}</b><br>
+                Population: ${usa_data[i].Population}</center></li>`)
+            }
+            return (data.join(''))
+        } 
+        )()
+    }
+    </ul> 
+    </div>`
+    document.body.append(parent_div)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+apiCall()
